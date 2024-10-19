fb385f89c19daa427561c450ee9716d6c25dbe14
MindfulCadet <dejameentrar101@gmail.com>
2024-10-19 15:50:51 -0400

    Add files via upload

---
+++ Guide.py
@@ -0,0 +1,17 @@
+# Variable que contiene un entero
+edad = 45
+
+# Flotante
+salario = 1456.8
+
+# String
+nombre = "John"
+
+print(edad)
+print(salario)
+print(nombre)
+
+# Si se redeclara una variable, se cambia por completo el valor que contenia
+edad = 18
+
+print("Despues de redeclarar:",edad)
